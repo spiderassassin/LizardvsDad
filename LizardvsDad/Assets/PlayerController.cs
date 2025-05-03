d754d4859e8cd3e3f1205f5a2dc76a6b1ca752fe
Dave Goel <davegoel29@gmail.com>
2025-05-03 17:14:49 -0600

    fixed controller movement

--- LizardvsDad/Assets/PlayerController.cs
+++ LizardvsDad/Assets/PlayerController.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Transform camera;
+    public Rigidbody rb;
     public float moveSpeed = 5f;
     public float rotationSpeed = 720f;
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
     public Animator animator;
     private float prev_speed;
 
@@ -17,20 +21,26 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-        if (vertical < 0)
-        {
-            vertical = 0f;
-        }
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        //if (vertical < 0)
+        //{
+            //vertical = 0f;
+        //}
 
         // Movement direction
-        Vector3 direction = new Vector3(0f, 0f, vertical).normalized;
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
         if (direction.magnitude >= 0.1f)
         {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + camera.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            Vector3 movDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            Move(movDir);
+
             animator.speed = prev_speed;
-            Move(direction);
         }
         else
         {
@@ -41,6 +51,6 @@ public class PlayerController : MonoBehaviour
     public void Move(Vector3 direction)
     {
         // Move forward
-        transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        rb.MovePosition(rb.position + direction * moveSpeed * Time.deltaTime);
     }
 }
